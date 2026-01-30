779f09b9c804487ae3d73c1ff5db0fb673a5a8e4
LautaroLeall <lautaroleal4@gmail.com>
2026-01-30 20:10:02 -0300

    app.jsx call all componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import BtnWhatsapp from './components/BtnWhatsapp'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Contact />
+      <Footer />
+      <BtnWhatsapp />
     </>
   )
 }
